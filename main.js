602758f28677dcddc42c47571fa15b8f6d243315
noavrd <noavrd@gmail.com>
2022-01-30 02:02:14 +0200

    add css

--- main.js
+++ main.js
@@ -5,17 +5,20 @@ const clearBtn = document.getElementById('clear');
 
 let savedItems = [];
 
+// Check on loading if there are banners saved
 document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('savedItems') === null) {
+  console.log(localStorage);
+  if (localStorage.getItem('banners-data') === null) {
     savedItems = [];
   } else {
-    savedItems = JSON.parse(localStorage.getItem('savedItems'));
+    savedItems = JSON.parse(localStorage.getItem('banners-data'));
     savedItems.map((item) => {
       createNewBanner(bannersList, item.redirectUrl, item.imgUrl);
     });
   }
 });
 
+// Add new banner on submit button
 submitBtn.onclick = () => {
   let imgLink = document.getElementsByClassName('img-link')[0];
   let redirectLink = document.getElementsByClassName('redirect-link')[0];
@@ -31,19 +34,23 @@ submitBtn.onclick = () => {
   redirectLink.value = '';
 };
 
+// Save banners to local storage on save button
 saveBtn.onclick = () => {
-  localStorage.setItem('savedItems', JSON.stringify(savedItems));
+  localStorage.setItem('banners-data', JSON.stringify(savedItems));
 };
 
+// Delete banners from local storage on clear button
 clearBtn.onclick = () => {
   const images = document.getElementsByClassName('single-banner');
   localStorage.clear();
-  // console.log(images);
-  // while (bannersList.lastElementChild) {
-  //   bannersList.removeChild(lastElementChild);
-  // }
+  console.log(images.length);
+  const num = images.length;
+  for (let i = 0; i < num; i++) {
+    images[0].remove();
+  }
 };
 
+// Add banners and display them on screen
 function createNewBanner(element_id, redirect_link, banner_img) {
   const imgDiv = document.createElement('div');
   const imgSource = document.createElement('img');
@@ -53,6 +60,8 @@ function createNewBanner(element_id, redirect_link, banner_img) {
   link.href = redirect_link;
   link.target = '_blank';
   imgSource.src = banner_img;
+  imgSource.className = 'img-banner';
+  imgSource.alt = 'url';
 
   imgSource.style.height = '320px';
   imgSource.style.width = '320px';
